ff1241a8a1b7ff3dd289d21cd23d671ad3b10175
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-04-15 22:59:05 -0500

    add blog post model

--- models.py
+++ models.py
@@ -11,14 +11,34 @@ class User(db.Model):
   '''User Model'''
   __tablename__ = 'users'
 
-  def __repr__(self):
-    u = self
-    return f'<User id={user_id} first_name={first_name} last_name={last_name}>'
-
   id = db.Column(db.Integer, primary_key = True, autoincrement = True)
 
   first_name = db.Column(db.String(20), nullable = False)
 
   last_name = db.Column(db.String(20), nullable = False)
 
-  img_url = db.Column(db.String(400), default = 'https://avatars.githubusercontent.com/u/76506172?s=60&v=4')
+  img_url = db.Column(db.Text, default = 'https://avatars.githubusercontent.com/u/76506172?s=60&v=4')
+
+  def __repr__(self):
+    u = self
+    return f'<User id={u.id} first_name={u.first_name} last_name={u.last_name}>'
+
+class Post(db.Model):
+  '''Post Model'''
+  __tablename__ = 'posts'
+
+  id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+
+  title = db.Column(db.Text, nullable = False)
+
+  content = db.Column(db.Text, nullable = False)
+
+  created_at = db.Column(db.DateTime(timezone=True), default = func.now)
+
+  user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+  author = db.relationship('User', backrefs='posts')
+  
+  def __repr__(self):
+    p = self
+    return f'<Post id={p.id} title={p.title} author={p.users.first_name} {p.users.last_name}>'
