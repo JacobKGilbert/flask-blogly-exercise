5ccb5f23dc786ca08463b54d6b39272d30effc00
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-04-19 19:41:47 -0500

    reconfigured blog-post routes

--- app.py
+++ app.py
@@ -46,12 +46,12 @@ def new_user_rt():
 def select_user_rt(user_id):
   '''Show details for single user.'''
   user = User.query.get_or_404(user_id)
-  posts = Post.query.filter_by(f'Post.author.id = {user_id}').all()
+  posts = Post.query.filter_by(user_id = user_id).all()
   return render_template('user_info.html', user=user, posts=posts)
 
 @app.route('/users/<int:user_id>/edit', methods=['GET', 'POST'])
-def user_edit_rt(user_id):
-  '''GET edit form. POST edits into database.'''
+def edit_user_rt(user_id):
+  '''GET edit_user form. POST edits to user into database.'''
   if request.method == 'POST':
     ed_user = User.query.get(user_id)
     ed_user.first_name = request.form['firstName']
@@ -64,7 +64,7 @@ def user_edit_rt(user_id):
     return redirect('/users')
   else:
     user = User.query.get(user_id)
-    return render_template('edit.html', user=user)
+    return render_template('edit_user.html', user=user)
 
 @app.route('/users/<int:user_id>/delete', methods=['POST'])
 def delete_user_rt(user_id):
@@ -79,14 +79,47 @@ def delete_user_rt(user_id):
 def new_post_rt(user_id):
   '''GET new post form. POST new blog post.'''
   if request.method == 'POST':
-    author = User.query.get(user_id)
+    user_id = User.query.get(user_id)
+    title = request.form['title']
+    content = request.form['content']
+    new_post = Post(title=title, content=content, user_id=user_id)
+
+    db.session.add(new_post)
+    db.session.commit()
     
     return redirect(f'/users/{user_id}')
   else:
-    return render_template('new_post_form.html')
+    user = User.query.get(user_id)
+    return render_template('new_post_form.html', user=user)
 
 @app.route('/posts/<int:post_id>')
 def select_post_rt(post_id):
   post = Post.query.get_or_404(post_id)
   return render_template('post.html', post=post)
 
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post_rt(post_id):
+  '''GET edit_post form. POST edits for blog post into database.'''
+  if request.method == 'POST':
+    ed_po = Post.query.get(post_id)
+    ed_po.title = request.form['title']
+    ed_po.content = request.form['content']
+
+    db.session.add(ed_po)
+    db.session.commit()
+
+    return redirect(f'/posts/{post_id}')
+  else:
+    post = User.query.get(post_id)
+    return render_template('edit_post.html', post=post)
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post_rt(post_id):
+  '''Delete select post.'''
+  post = Post.query.get(post_id)
+  user_id = post.author.id
+
+  post.delete()
+
+  db.session.commit()
+  return redirect(f'/users/{user_id}')
